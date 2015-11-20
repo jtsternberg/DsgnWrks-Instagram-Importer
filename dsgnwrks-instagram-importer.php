78500410d010e14e628e89b382bed70b74322fb5
Justin Sternberg <justin@dsgnwrks.pro>
2015-11-19 23:35:52 -0500

    bump version in plugin header

--- dsgnwrks-instagram-importer.php
+++ dsgnwrks-instagram-importer.php
@@ -6,12 +6,12 @@ Description: Allows you to backup your instagram photos while allowing you to ha
 Author URI: http://dsgnwrks.pro
 Author: DsgnWrks
 Donate link: http://dsgnwrks.pro/give/
-Version: 1.3.5
+Version: 1.3.6
 */
 
 class DsgnWrksInstagram extends DsgnWrksInstagram_Debug {
 
-	public $plugin_version = '1.3.5';
+	public $plugin_version = '1.3.6';
 	public $plugin_id      = 'dsgnwrks-instagram-importer-settings';
 	public $pre            = 'dsgnwrks_instagram_';
 	public $instagram_api  = 'https://api.instagram.com/v1/users/';
